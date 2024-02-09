88ec562270ecca2f907ba1fce1c3572addfa607a
pmahend1 <prateek.mahendrakar@gmail.com>
2024-02-08 20:54:33 -0500

    webpack fix

--- webpack.config.js
+++ webpack.config.js
@@ -8,11 +8,14 @@ const path = require('path');
 const config = {
     target: 'node', // vscode extensions run in a Node.js-context 📖 -> https://webpack.js.org/configuration/node/
 
-    entry: './src/extension.ts', // the entry point of this extension, 📖 -> https://webpack.js.org/configuration/entry-context/
+    entry: {
+        extension: './src/extension.ts',
+        webpanelScript: './src/webpanelScript.ts'
+    },// the entry point of this extension, 📖 -> https://webpack.js.org/configuration/entry-context/
     output: {
         // the bundle is stored in the 'dist' folder (check package.json), 📖 -> https://webpack.js.org/configuration/output/
         path: path.resolve(__dirname, 'out'),
-        filename: 'extension.js',
+        filename: '[name].js',
         libraryTarget: 'commonjs2',
         devtoolModuleFilenameTemplate: '../[resource-path]'
     },
