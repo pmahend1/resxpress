0ca9179540584b860cfd4ba2b452e137244a11f7
Prateek Mahendrakar <prateek.mahendrakar@gmail.com>
2025-09-27 18:57:04 -0400

    Potential fix for code scanning alert no. 7: Client-side cross-site scripting (#122)

--- src/webpanelScript.ts
+++ src/webpanelScript.ts
@@ -20,6 +20,7 @@ const text = "text";
 const click = "click";
 const deleteStr = "delete";
 const X = "X";
+const strong = "strong";
 const errorDuplicateKey = (key: string) => `Error: Data with ${key} already exists`;
 const errorInvalidResx = "Error: Document is not valid resx";
 const errorKeyValueMandatory = "Key and Value are both mandatory fields!";
@@ -399,7 +400,10 @@ function logToConsole(text: string) {
 				const newNamespace = text;
 				const namespaceSpanElement = document.getElementById(namespaceSpan);
 				if (namespaceSpanElement) {
-					namespaceSpanElement.innerHTML = `Namespace: <strong>${newNamespace}</strong>`;
+					namespaceSpanElement.innerHTML = "Namespace: ";
+					const strongElement = document.createElement(strong);
+					strongElement.textContent = newNamespace;
+					namespaceSpanElement.appendChild(strongElement);
 				}
 				break;
 		}
